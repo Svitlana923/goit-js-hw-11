db462afc0d4cadd8bc768be2595ebac492afc354
Svitlana923 <svitlanakostiuk11@gmail.com>
2024-02-20 00:17:48 +0100

    2

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -4,6 +4,38 @@ import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+const userValue = document.querySelector('input').value;
+
+function searchImages(userValue) {
+    const BASE_URL = 'https://pixabay.com/api/'
+    const key = '?key=42424645-ecd3f1048329df1dec069e6a8&'
+    const q = `q=${userValue}&`
+    const PARAMS = 'image_type=photo&orientation=horizontal&safesearch=true&per_page=18'
+    const url = BASE_URL + key + q + PARAMS;
+
+    return fetch(url)
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw new Error();
+            }
+        })
+        .then(data => {
+        if (data.hits.length === 0) {
+            throw new Error('No results found');
+        }
+        return data;
+    })
+        .catch(error => {
+            iziToast.error({
+                title: '',
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+                position: 'topRight'
+            });
+        });
+}
+
   const lightbox = new SimpleLightbox('.gallery a', {
     captionDelay: 250,
     });
