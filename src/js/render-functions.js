addf50110c01d7f83b490a40a25c80966c02d892
Svitlana923 <svitlanakostiuk11@gmail.com>
2024-02-21 19:36:29 +0100

    3

---
+++ src/js/render-functions.js
@@ -0,0 +1,34 @@
+
+function renderImages(array) {
+       const galleryEl = document.querySelector('.gallery-o');
+    const markup = array
+        .map(
+        ({
+            largeImageURL,
+            webformatURL,
+            tags,
+            likes,
+            views,
+            comments,
+            downloads,
+        }) => {
+            return `
+        <div class="gallery">
+            <a href="${largeImageURL}">
+            <img src="${webformatURL}" alt="${tags}" title="${tags}" width="380" height="220" />
+            <ul class="info-cards-container">
+                <li class="info-cards-elements">likes<span>${likes}</span></li>
+                <li class="info-cards-elements">views<span>${views}</span></li>
+                <li class="info-cards-elements">comments<span>${comments}</span></li>
+                <li class="info-cards-elements">downloads<span>${downloads}</span></li>
+            </ul>
+            </a>
+        </div>
+        `;
+        }
+        )
+        .join('');
+
+    galleryEl.innerHTML = markup;   
+}
+    
